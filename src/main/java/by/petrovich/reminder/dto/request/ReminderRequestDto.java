1b21691a7a4bae422f498e5a0ef01d9776fe7cba
Petrovich Alexandr <a.piatrovich@gmail.com>
2024-07-03 18:17:49 +0300

    minor fix

--- src/main/java/by/petrovich/reminder/dto/request/ReminderRequestDto.java
+++ src/main/java/by/petrovich/reminder/dto/request/ReminderRequestDto.java
@@ -6,10 +6,10 @@ import lombok.Data;
 @Data
 @Schema(description = "DTO for creating a reminder")
 public class ReminderRequestDto {
-    @Schema(description = "Title of the reminder", example = "title3")
+    @Schema(description = "Title of the reminder", example = "Meeting")
     private String title;
 
-    @Schema(description = "Description of the reminder", example = "description3")
+    @Schema(description = "Description of the reminder", example = "Team meeting at 10 AM")
     private String description;
 
     @Schema(description = "ID of the user who created the reminder", example = "3")
