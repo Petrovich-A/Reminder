080b109d629adedb4ee5eabcbf2a30c012974a72
Petrovich Alexandr <A.Piatrovich@gmail.com>
2024-08-14 11:33:33 +0300

    fixed loading docs using openapi.yaml :memo:

--- src/main/java/by/petrovich/reminder/controller/impl/ReminderControllerImpl.java
+++ src/main/java/by/petrovich/reminder/controller/impl/ReminderControllerImpl.java
@@ -60,9 +60,9 @@ public class ReminderControllerImpl {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable @Min(1) Long id) {
+    public ResponseEntity<String> delete(@PathVariable @Min(1) Long id) {
         reminderService.delete(id);
-        return ResponseEntity.status(NO_CONTENT).build();
+        return ResponseEntity.status(NO_CONTENT).body("Reminder deleted");
     }
 
     @PutMapping("/{id}")
